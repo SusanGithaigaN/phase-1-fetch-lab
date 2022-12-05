4b50e679e6f45ff951fbcd0a99c42917eb058385
SusanGithaiga <susan.githaiga@student.moringaschool.com>
2022-12-05 10:58:37 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  // Send a fetch request to retrieve resources from the server.
+  return (
+    fetch("https://anapioficeandfire.com/api/books")
+      // Initialize a new response object.
+      .then(function (response) {
+        // Retrieve the response as .json.
+        return response.json();
+      })
+      // Render book titles into the DOM by passing a JSON object to renderBooks().
+      .then(function (json) {
+        renderBooks(json);
+      })
+  );
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
 });
